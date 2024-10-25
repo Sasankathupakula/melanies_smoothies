817142efd9aa230c9f85f7d6ac4639cc9818c389
Sasankathupakula <sasanka.thupakula@sdggroup.com>
2024-10-25 12:15:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,12 @@ if ingredients_list:
        
          st.success('Your Smoothie is ordered!', icon="✅")
         
-
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+if fruityvice_response.content:
+    fruityvice_data = fruityvice_response.json()
+    st.text(fruityvice_data)
+else:
+    st.text("Error: Received empty response from the API")
 
 
 
